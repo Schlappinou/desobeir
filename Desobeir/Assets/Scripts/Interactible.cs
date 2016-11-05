e7dbd10099e642789cc43fcba7284c08e21a34c1
ChatMalHaut <antonin.rousset@telecom-sudparis.eu>
2016-11-05 14:20:00 +0100

    architecture + door

---
+++ Desobeir/Assets/Scripts/Interactible.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Interactible : MonoBehaviour {
+
+    // Use this for initialization
+    float interactionRadius;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public abstract void Interaction();
+
+    public abstract bool isInRange();
+
+
+
+}
