4dbb06fbe65e9d4425ce9e45e8d8be6ba8bc8f19
Schlappinou <desbonnets.theo@gmail.com>
2016-11-05 02:20:59 +0100

    Add files via upload

---
+++ Desobeir/Assets/PlayerAnimator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimator : MonoBehaviour {
+
+
+    Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        // find one time for all the animator attached to this gameobject + Debug
+        animator = GetComponent<Animator>(); 
+        if(animator == null)
+        {
+            Debug.Log("Animator is null for "+transform.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // here value of some imput
+        //< float | bool |...> -value to be set- = Input.GetAxis(String name of Imput);
+
+
+        if (animator != null)
+        {
+            //here set of value of animator parameters 
+            //animator.SetFloat(string parameter name, <float|bool|...> value to be set);
+
+        }
+
+    }
+}
