8013400260aea79365b53160e44b91a86b6c8cf9
Schlappinou <desbonnets.theo@gmail.com>
2016-11-06 09:29:05 +0100

    Add files via upload

---
+++ Desobeir/Assets/Scripts/Bouton_Ass.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bouton_Ass : Interactible
+{
+
+    // Use this for initialization
+    [SerializeField]
+    int nombreAppuiPossible;
+    [SerializeField]
+    int NumeroClipAudio;
+    [SerializeField]
+    string nextScene;
+    [SerializeField]
+    bool hasNextScene;
+    [SerializeField]
+    float obeissance;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Interaction()
+    {
+        if (nombreAppuiPossible >= 1)
+        {
+            GameManager.instance.addJauge(obeissance);
+            nombreAppuiPossible -= 1;
+            if (nombreAppuiPossible == 0)
+            {
+                Debug.Log("interaction faite");
+                //SceneMain.instance.ReadVoice (NumeroClipAudio);
+                
+            }
+        }
+
+
+        if (hasNextScene)
+        {
+            GameManager.instance.sceneLoader(nextScene);
+        }
+
+    }
+}
