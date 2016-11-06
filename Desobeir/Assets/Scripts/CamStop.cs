217fa7cd48b6437aa913d9cf1995e859bce4078d
JBerthel <jb.berthelin@gmail.com>
2016-11-06 13:27:22 +0100

    Final (manque interaction)

---
+++ Desobeir/Assets/Scripts/CamStop.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CamStop : MonoBehaviour {
+	public Camera cam;
+	private RailCam Rc;
+	// Use this for initializatio
+	void Start () {
+		Rc =cam.GetComponent<RailCam> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerStay2D(Collider2D other){
+		Rc.enabled = false;
+	}
+}
