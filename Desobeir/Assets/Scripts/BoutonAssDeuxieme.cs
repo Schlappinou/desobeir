e9293b4ab77870eeb81768ea69e71e50d6a5acc1
ChatMalHaut <antonin.rousset@telecom-sudparis.eu>
2016-11-06 14:48:39 +0100

    Add files via upload

---
+++ Desobeir/Assets/Scripts/BoutonAssDeuxieme.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoutonAssDeuxieme : Interactible
+{
+
+    // Use this for initialization
+    [SerializeField]
+    int nombreAppuiPossible;
+    [SerializeField]
+    int NumeroClipAudio;
+    [SerializeField]
+    string nextScene;
+    [SerializeField]
+    bool hasNextScene;
+    [SerializeField]
+    float obeissance;
+    
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Interaction()
+    {
+        if (nombreAppuiPossible >= 1)
+        {
+            GameManager.instance.addJauge(obeissance);
+            nombreAppuiPossible -= 1;
+            if (nombreAppuiPossible == 0)
+            {
+                Debug.Log("interaction faite");
+                SceneMain.instance.ReadVoice (NumeroClipAudio);
+
+            }
+        }
+
+
+        if (hasNextScene)
+        {
+            SceneMain.instance.Tuyauxpris = true;
+            Invoke("StartNextScene",10f);
+        }
+
+    }
+
+    void StartNextScene()
+    {
+        GameManager.instance.sceneLoader(nextScene);
+    }
+}
